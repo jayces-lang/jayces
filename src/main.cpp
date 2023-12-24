89fe43306c60b7ecc4a357e835db4af7c832326d
JenChieh <jcs090218@gmail.com>
2023-12-24 01:56:41 -0800

    Add build workflow

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,19 @@
  * Copyright 2023 the JayCeS authors. All rights reserved. MIT license.
  */
 
+#include <cstring>
+#include <iostream>
+#include <CLI/CLI.hpp>
+
 #include <llvm/ADT/Optional.h>
 
 auto main(int argc, char** argv) -> int {
+    CLI::App app{"App description"};
+    char** original_argv = argv;
 
+    std::string filename = "default";
+    app.add_option("-f,--file", filename, "A help string");
+
+    CLI11_PARSE(app, argc, argv);
     return 0;
 }
