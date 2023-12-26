c0b5ca51e63d22a19516ff8e86499540f0912fd1
JenChieh <jcs090218@gmail.com>
2023-12-26 03:35:17 -0800

    Add comment

--- include/Env.hpp
+++ include/Env.hpp
@@ -5,5 +5,20 @@
  */
 #pragma once
 
+/**
+ * @def JAYCES_APP_DESC
+ * @brief Description shown in CLI.
+ */
 #define JAYCES_APP_DESC ""
+
+/**
+ * @def JAYCES_VERSION
+ * @brief Version number.
+ */
 #define JAYCES_VERSION "0.1.0"
+
+/**
+ * @def JAYCES_EXTENSION
+ * @brief Programming language file extension
+ */
+#define JAYCES_EXTENSION ".jcs"
