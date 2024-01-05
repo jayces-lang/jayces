266f6bfff86493ef1327ac5eb47d2fc4d859c8e9
JenChieh <jcs090218@gmail.com>
2024-01-05 03:00:46 -0800

    rm

--- scripts/build_cmake.bat
+++ scripts/build_cmake.bat
@@ -16,6 +16,6 @@ echo.
 echo ::Gathering compilation database information!
 copy build\ninja\compile_commands.json .
 
-echo.
-echo ::Build MSVC solution
-cmake -S . -B build/msvc/
+::echo.
+::echo ::Build MSVC solution
+::cmake -S . -B build/msvc/
