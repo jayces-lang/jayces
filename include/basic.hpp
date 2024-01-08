76d8b7c17bdc68d92f12717f69daf01c993aae90
JenChieh <jcs090218@gmail.com>
2024-01-08 03:40:42 -0800

    style: Format with clang-format

--- include/basic.hpp
+++ include/basic.hpp
@@ -7,14 +7,13 @@
 
 #include <unordered_map>
 
-namespace jayces
+namespace jayces {
+template <class T, class K>
+std::unordered_map<T, K> reverse_map(std::unordered_map<K, T> map)
 {
-    template <class T, class K>
-    std::unordered_map<T, K> reverse_map(std::unordered_map<K, T> map)
-    {
-        std::unordered_map<T, K> result;
-        for (const auto &p : map)
-            result[p.second] = p.first;
-        return result;
-    }
+    std::unordered_map<T, K> result;
+    for (const auto &p : map)
+        result[p.second] = p.first;
+    return result;
 }
+} // namespace jayces
